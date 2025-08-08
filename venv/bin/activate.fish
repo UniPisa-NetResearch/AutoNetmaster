dfc1e993f4c47aa2643a81f9ba21f69f23b3eacc
DanieleCongiusti <daniele@DANYX.localdomain>
2025-08-08 08:16:58 +0200

    Commit per cartella virtual environment di python

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/lorenzo/ospf-mgmt-app/venv
+set -gx VIRTUAL_ENV /home/daniele/AutoNetMaster/AutoNetmaster/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
